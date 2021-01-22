fd5fc267fe92456439921ac9bbc2a9254906efc1
Thomas <thomasbulle@hotmail.fr>
2021-01-22 15:29:42 +0100

    Add henri potier library project

---
+++ precache-manifest.0530addf0cb3a73361d4ba9e34902aed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf6b8ab143db3f19dba9e319c14dd79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fbeda352a8bbb6c2f2",
+    "url": "/static/css/2.55141b4d.chunk.css"
+  },
+  {
+    "revision": "839b4448cfdd06949a27",
+    "url": "/static/css/main.5850bcdd.chunk.css"
+  },
+  {
+    "revision": "72fbeda352a8bbb6c2f2",
+    "url": "/static/js/2.f16a87e8.chunk.js"
+  },
+  {
+    "revision": "e891163eb803720851df9e3d2110b1c3",
+    "url": "/static/js/2.f16a87e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839b4448cfdd06949a27",
+    "url": "/static/js/main.85490180.chunk.js"
+  },
+  {
+    "revision": "a0e1745394e851035fbd",
+    "url": "/static/js/runtime-main.5164bbac.js"
+  }
+]);
